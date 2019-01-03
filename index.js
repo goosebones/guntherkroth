dc5dadfd180766fba119d38045a85d63de1255ed
Gunther Kroth <gdkroth@gmail.com>
2019-01-03 15:44:03 -0500

    Icons and name

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+//$('#square').mouseenter(function() {
+//  $('#square').hide();
+//)};
+$("#square").on("click", function() {
+  $("#square").hide();
+});
+alert("thadeous")
+
+});
